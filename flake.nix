43380f8af86e606a9143577ea505d9956fe0912f
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-10-23 16:31:44 +0200

    Add TU logo to patched derivation

--- flake.nix
+++ flake.nix
@@ -23,6 +23,16 @@
             mkdir -p $out/tex/latex/algotex
             cp -t $out/tex/latex/algotex/ $src/tex/*
             cp $out/tex/latex/algotex/FOPBot.sty $out/tex/latex/algotex/fopbot.sty
+
+            mkdir -p $out/tex/latex/local
+            rsvg-convert -f pdf -o $out/tex/latex/local/tuda_logo.pdf ${
+              builtins.fetchurl {
+                url =
+                  "https://upload.wikimedia.org/wikipedia/de/2/24/TU_Darmstadt_Logo.svg";
+                sha256 =
+                  "sha256:0wb2ygg0wzd2ajdzv847xp1qxkx4m1j64p1gdrq4r05klbrddbwl";
+              }
+            }
           '';
         });
       patched-latex = with pkgs.texlive;
